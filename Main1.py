984ba3982f1631f57a3ff8fc5190cf78a005b491
Вулкан <egovigor@gmail.com>
2022-07-17 19:23:42 +0300

    Добавили арифметические операции

--- Main1.py
+++ Main1.py
@@ -1,15 +1,17 @@
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+msg_3 = "Yeah... division by zero. Smart move..."
 sign = ['-', '+', '*', '/']
+result = 0
 
 while True:
     print(msg_0)
     x, oper, y = input().split()
 
     try:
-        float(x)
-        float(y)
+        x = float(x)
+        y = float(y)
     except ValueError:
         print(msg_1)
         continue
@@ -18,4 +20,21 @@ while True:
         print(msg_2)
         continue
 
+    if oper == '+':
+        result = x + y
+    elif oper == '-':
+        result = x - y
+    elif oper == '*':
+        result = x * y
+    elif oper == '/':
+        if y == 0:
+            print(msg_3)
+            continue
+        else:
+            result = x / y
+
+    print(result)
+
     break
+
+
