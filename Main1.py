00c8b0fca7e2e3f7f7e7f3f4ff1ddee5181fcf53
Вулкан <egovigor@gmail.com>
2022-07-18 23:14:48 +0300

    Добавил в калькуляторе возможность запоминать результат

--- Main1.py
+++ Main1.py
@@ -2,12 +2,20 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
 sign = ['-', '+', '*', '/']
 result = 0
+memory = 0.0
 
 while True:
     print(msg_0)
     x, oper, y = input().split()
+
+    if x == 'M':
+        x = memory
+    if y == 'M':
+        y = memory
 
     try:
         x = float(x)
@@ -35,6 +43,29 @@ while True:
 
     print(result)
 
+    while True:
+        answer = input(msg_4)
+        if answer == 'y':
+            memory = result
+            break
+        elif answer == 'n':
+            break
+        else:
+            continue
+
+    start = False
+    while True:
+        answer = input(msg_5)
+        if answer == 'y':
+            start = True
+            break
+        elif answer == 'n':
+            break
+        else:
+            continue
+
+    if start:
+        continue
     break
 
 
