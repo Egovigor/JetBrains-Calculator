429b38e7a4bf7b8e1fe2a72755ade261f52c8bba
Вулкан <egovigor@gmail.com>
2022-07-19 18:08:39 +0300

    Добавил функцию в зависимости от числа добавляется тект ленивый

--- Main1.py
+++ Main1.py
@@ -4,9 +4,35 @@ msg_2 = "Yes ... an interesting math operation. You've slept through all classes
 msg_3 = "Yeah... division by zero. Smart move..."
 msg_4 = "Do you want to store the result? (y / n):"
 msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = "You are"
 sign = ['-', '+', '*', '/']
 result = 0
 memory = 0.0
+
+
+def is_one_digit(v) -> bool:
+    if v > -10 and v < 10 and v.is_integer():
+        return True
+    else:
+        return False
+
+
+def check(v1, v2, v3):
+    msg = ""
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg += msg_6
+    if (v1 == 1 or v2 == 1) and v3 == "*":
+        msg += msg_7
+    if (v1 == 0 or v2 == 0) and (v3 == "*" or v3 == "+" or v3 == "-"):
+        msg += msg_8
+    if msg != "":
+        msg = msg_9 + msg
+        print(msg)
+
+
 
 while True:
     print(msg_0)
@@ -27,6 +53,8 @@ while True:
     if oper not in sign:
         print(msg_2)
         continue
+
+    check(x, y, oper)
 
     if oper == '+':
         result = x + y
@@ -67,5 +95,3 @@ while True:
     if start:
         continue
     break
-
-
