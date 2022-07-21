b19565e1ffd3e1e219f1101b7678b02eee1986ac
Вулкан <egovigor@gmail.com>
2022-07-21 21:43:38 +0300

    Замена сообщений с отдельных переменных. На словарь

--- Main1.py
+++ Main1.py
@@ -1,41 +1,44 @@
-msg_0 = "Enter an equation"
-msg_1 = "Do you even know what numbers are? Stay focused!"
-msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
-msg_3 = "Yeah... division by zero. Smart move..."
-msg_4 = "Do you want to store the result? (y / n):"
-msg_5 = "Do you want to continue calculations? (y / n):"
-msg_6 = " ... lazy"
-msg_7 = " ... very lazy"
-msg_8 = " ... very, very lazy"
-msg_9 = "You are"
+msg = {0: "Enter an equation",
+       1: "Do you even know what numbers are? Stay focused!",
+       2: "Yes ... an interesting math operation. You've slept through all classes, haven't you?",
+       3: "Yeah... division by zero. Smart move...",
+       4: "Do you want to store the result?  y / n):",
+       5: "Do you want to continue calculations?  y / n):",
+       6: " ... lazy",
+       7: " ... very lazy",
+       8: " ... very, very lazy",
+       9: "You are",
+       10: "Are you sure? It is only one digit! (y / n)",
+       11: "Don't be silly! It's just one number! Add to the memory? (y / n)",
+       12: "Last chance! Do you really want to embarrass yourself? (y / n)"}
+
 sign = ['-', '+', '*', '/']
 result = 0
 memory = 0.0
 
 
 def is_one_digit(v) -> bool:
-    if v > -10 and v < 10 and v.is_integer():
+    if -10 < v < 10 and v.is_integer():
         return True
     else:
         return False
 
 
 def check(v1, v2, v3):
-    msg = ""
+    message = ""
     if is_one_digit(v1) and is_one_digit(v2):
-        msg += msg_6
+        message += msg[6]
     if (v1 == 1 or v2 == 1) and v3 == "*":
-        msg += msg_7
+        message += msg[7]
     if (v1 == 0 or v2 == 0) and (v3 == "*" or v3 == "+" or v3 == "-"):
-        msg += msg_8
-    if msg != "":
-        msg = msg_9 + msg
-        print(msg)
-
+        message += msg[8]
+    if message != "":
+        message = msg[9] + message
+        print(message)
 
 
 while True:
-    print(msg_0)
+    print(msg[0])
     x, oper, y = input().split()
 
     if x == 'M':
@@ -47,11 +50,11 @@ while True:
         x = float(x)
         y = float(y)
     except ValueError:
-        print(msg_1)
+        print(msg[1])
         continue
 
     if oper not in sign:
-        print(msg_2)
+        print(msg[2])
         continue
 
     check(x, y, oper)
@@ -64,7 +67,7 @@ while True:
         result = x * y
     elif oper == '/':
         if y == 0:
-            print(msg_3)
+            print(msg[3])
             continue
         else:
             result = x / y
@@ -72,26 +75,27 @@ while True:
     print(result)
 
     while True:
-        answer = input(msg_4)
+        answer = input(msg[4])
         if answer == 'y':
+        else:
             memory = result
-            break
-        elif answer == 'n':
-            break
-        else:
-            continue
 
-    start = False
-    while True:
-        answer = input(msg_5)
-        if answer == 'y':
-            start = True
-            break
         elif answer == 'n':
-            break
-        else:
-            continue
-
-    if start:
+        break
+    else:
         continue
-    break
+
+start = False
+while True:
+    answer = input(msg[5])
+    if answer == 'y':
+        start = True
+        break
+    elif answer == 'n':
+        break
+    else:
+        continue
+
+if start:
+    continue
+break
